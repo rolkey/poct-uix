9dc25af6c95783535ecc3051de8fc54689dcadc5
马海军 <517409926@qq.com>
2026-09-03 00:30:45 +0800

    简化代码

--- packages/shared/src/utils/request.ts
+++ packages/shared/src/utils/request.ts
@@ -1,25 +1,25 @@
-import { AxiosPromise } from "axios";
-
 const msgBus = (): IMessageBus => (window as any).__QIANKUN_MSG_BUS__ as IMessageBus;
 
 // 构建请求 Promise, 通过调用MsgBus来发送请求
-function request<T = any>(options: RequestOptions): AxiosPromise<T> {
+function request<T = any>(options: RequestOptions): Promise<T> {
   // 生成一个随机数
   const requestId = Math.random().toString(36);
   options.requestId = requestId;
   return new Promise((resolve, reject) => {
+    const removeListener = () => {
+      msgBus().off("c_response_" + requestId, successCallback);
+      msgBus().off("c_response_fail_" + requestId, failCallback);
+    };
     // 定义成功和失败的回调函数
     const successCallback = (res: any) => {
       // 移除监听器
-      msgBus().off("c_response_" + requestId, successCallback);
-      msgBus().off("c_response_fail_" + requestId, failCallback);
+      removeListener();
       resolve(res);
     };
 
     const failCallback = (res: any) => {
       // 移除监听器
-      msgBus().off("c_response_" + requestId, successCallback);
-      msgBus().off("c_response_fail_" + requestId, failCallback);
+      removeListener();
       reject(res);
     };
 
