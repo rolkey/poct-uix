275da42e5cfbad46896b1416582a8fb3a2e3f0af
马海军 <517409926@qq.com>
2026-09-04 07:08:58 +0800

    类型匹配问题

--- packages/shared/src/types/axios.d.ts
+++ packages/shared/src/types/axios.d.ts
@@ -1,6 +1,7 @@
 export {};
 declare module 'axios' {
-  interface AxiosResponse<T = any> {
+  // 泛型参数需与 axios 1.x 声明完全一致(H 为响应头泛型),否则接口合并失效
+  interface AxiosResponse<T = any, D = any, H = {}> {
     code: number;
     msg: string;
     rows: T;
